e62786e22b33b5fb14d25ead41165e554bbc243c
BuildTools <unconfigured@null.spigotmc.org>
2022-08-28 20:18:24 +0530

    Added favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import '../styles/globals.css'
 import Appbar from './components/Appbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,6 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>AcidOP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Appbar />
       <Component {...pageProps} />
     </>

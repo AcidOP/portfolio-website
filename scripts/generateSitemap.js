6c4be1ebcecc08bd309871f1d55336e772277648
AcidOP <zeeshanalivr46@gmail.com>
2022-09-16 21:19:02 +0530

    improved seo

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,34 @@
+const { readdirSync, writeFileSync } = require('fs');
+
+const rootDir = process.cwd();
+const projectDir = rootDir + '/markdowns/projects';
+
+const files = readdirSync(projectDir);
+const markdowns = files.filter(file => file.endsWith('.md'));
+
+const slugs = markdowns.map(file => file.replace('.md', ''));
+const sitemapUrls = slugs.map(slug => `https://acidop.codes/projects/${slug}`);
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+    <loc>https://acidop.codes/</loc>
+</url>
+<url>
+    <loc>https://acidop.codes/about</loc>
+</url>
+<url>
+    <loc>https://acidop.codes/projects</loc>
+</url>
+${sitemapUrls.map(url => `
+<url>
+    <loc>${url}</loc>
+</url>
+`).join('')}
+<url>
+    <loc>https://acidop.codes/contact</loc>
+</url>
+</urlset>
+`;
+
+writeFileSync('public/sitemap.xml', sitemap);
